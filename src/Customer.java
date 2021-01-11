01d778950f09b20851e77471bf4fee886563a05e
heiye5 <2716634948@qq.com>
2021-01-11 15:13:38 +0800

    Initial commit

---
+++ src/Customer.java
@@ -0,0 +1,38 @@
+public class Customer {
+    private String id;
+    private String customerName;
+    private String customerNumber;
+    private String customerCardId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCustomerCardId() {
+        return customerCardId;
+    }
+
+    public void setCustomerCardId(String customerCardId) {
+        this.customerCardId = customerCardId;
+    }
+}

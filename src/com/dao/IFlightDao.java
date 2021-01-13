e671de97286a2116771a638a8c2a5fe55a393bef
heiye5 <2716634948@qq.com>
2021-01-13 10:03:38 +0800

    插入数据库

--- src/com/dao/IFlightDao.java
+++ src/com/dao/IFlightDao.java
@@ -1,10 +1,12 @@
 package com.dao;
 
 import com.bean.FlightInfo;
+
+import java.sql.SQLException;
 import java.util.Set;
 
 public interface IFlightDao {
-    void insertFlight(FlightInfo flightInfo);
+    void insertFlight(FlightInfo flightInfo) throws SQLException;
     Set<FlightInfo> getAllFlight();
     FlightInfo getFlightByDepartureCity(String departureCity);
     FlightInfo getFlightByArriveCity(String arriveCity);
